c478d55cd3326b46b103d1045821949314a032b2
web-solution1 <ammar.b6507187@gmail.com>
2023-08-04 04:48:15 +0300

    Advanced tasks upload

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,12 +14,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-			_putchar(45);
-			m = n * -1;
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-			m = n;
+		m = n;
 	}
 
 	d = m;
@@ -27,12 +27,12 @@ void print_number(int n)
 
 	while (d > 9)
 	{
-			d /= 10;
-			count *= 10;
+		d /= 10;
+		count *= 10;
 	}
 
 	for (; count >= 1; count /= 10)
 	{
-			_putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
