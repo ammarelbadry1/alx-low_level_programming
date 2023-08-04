a28f3584e28ba5c0b0a3e87cc36c695b7a8ef80e
web-solution1 <ammar.b6507187@gmail.com>
2023-08-04 04:26:53 +0300

    Advanced tasks upload

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	long int i, n = 612852475143;
 
-	while (n%2 == 0)
+	while ((n % 2) == 0)
 	{
-		n = n/2;
+		n = n / 2;
 	}
 
-	for (i = 3; i <= sqrt(n); i = i+2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while (n%i == 0)
+		while ((n % i) == 0)
 		{
-			n = n/i;
+			n = n / i;
 		}
 	}
 
