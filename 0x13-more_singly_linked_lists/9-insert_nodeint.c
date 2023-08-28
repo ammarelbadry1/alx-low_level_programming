661680c24a778cd5592d16f85648deda21731714
web-solution1 <ammar.b6507187@gmail.com>
2023-08-29 00:55:05 +0300

    bug fix 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current, *new, *temp;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
 	current = *head;
 	i = 0;
 	while (i < idx && current != NULL)
